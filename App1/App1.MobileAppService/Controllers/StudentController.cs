4a59c3dc4a4c694ae6522497a39ebe2f629172d0
Neeraj Rai <Neeraj.Rai@ExcelliMatrix.com>
2019-04-12 19:11:05 +0530

    New Controller

---
+++ App1/App1.MobileAppService/Controllers/StudentController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace App1.MobileAppService.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/Student")]
+  public class StudentController : Controller
+  {
+
+    // GET: api/Student
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return ListStudents.ToArray();
+    }
+
+    // GET: api/Student/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST: api/Student
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT: api/Student/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+
+    public StudentController()
+    {
+      ListStudents.Add("Amit");
+      ListStudents.Add("Vinay");
+      ListStudents.Add("Mohan");
+      ListStudents.Add("Sundar");
+      ListStudents.Add("Raj");
+      ListStudents.Add("Satya");
+      ListStudents.Add("Krishna");
+    }
+
+    List<string> ListStudents = new List<string>();
+
+  }
+}
---
+++ App1/App1.MobileAppService/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App1.MobileAppService.Models
+{
+  public class Student
+  {
+    public int RollNumber { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string AddressLine { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+  }
+}
